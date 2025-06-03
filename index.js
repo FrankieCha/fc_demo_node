40e7204479e9b210e9acc5d0e9062c8e4adaf80d
Frankie Cha <frankie_cha@rp.edu.sg>
2025-06-03 22:44:19 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+// Placeholder function for fc_demo_node project
+
+function placeholder() {
+    // TODO: Implement functionality
+    console.log('This is a placeholder function.');
+}
+
+// Call the placeholder function
+placeholder();
